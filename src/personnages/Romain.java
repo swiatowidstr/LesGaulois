88b1f8298ae56464a5b9a1dd1c0f71e7640ce0d3
swiato <swiatowid.struski@gmail.com>
2025-10-09 17:09:36 +0200

    invariants, pré-cond et post-cond

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,27 +3,47 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified() : "La force doit être positive.";
+	}
+
+	private boolean isInvariantVerified() {
+		return (force >= 0);
 	}
 
 	public String getNom() {
 		return nom;
 	}
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
+
 	public String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
+
 	public void recevoirCoup(int forceCoup) {
+		assert forceCoup >= 0 : "La force du coup doit etre positive .";
+
+		int ancienneForce = force;
+
 		force -= forceCoup;
 		if (force <= 0) {
 			parler("j'abandonne ! ");
 		} else {
-			parler("Aie");
+			parler("Aïe");
 		}
+
+		assert force <= ancienneForce : "La force doit avoir diminué après avoir pris un coup .";
+
+		assert isInvariantVerified() : "La force doit être positive .";
+	}
+
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
 	}
 }
